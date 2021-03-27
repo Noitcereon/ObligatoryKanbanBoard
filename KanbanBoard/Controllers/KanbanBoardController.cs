cb3883b86c01455d4efc363472e5f6d744abe2c4
Thomas (Noit) <tba@live.dk>
2021-03-27 14:30:50 +0100

    Made KanbanBoardController. Ready for next time's work.

---
+++ KanbanBoard/Controllers/KanbanBoardController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KanbanBoardMVCApp.Data;
+using KanbanBoardMVCApp.Models;
+
+namespace KanbanBoardMVCApp.Controllers
+{
+    public class KanbanBoardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KanbanBoardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: KanbanBoards
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.KanbanBoards.ToListAsync());
+        }
+
+     
+    }
+}
