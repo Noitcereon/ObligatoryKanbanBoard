276a5318d79d588573b9db0f4d83de4eef7c2ece
Thomas (Noit) <tba@live.dk>
2021-03-30 14:58:57 +0200

    Made method async to fix issue. MoveItem confirmed to work.

--- KanbanBoard/Controllers/KanbanBoardController.cs
+++ KanbanBoard/Controllers/KanbanBoardController.cs
@@ -50,9 +50,9 @@ namespace KanbanBoardMVCApp.Controllers
         }
 
 
-        public IActionResult MoveItem(int itemId)
+        public async Task<IActionResult> MoveItem(int itemId)
         {
-            _repos.MoveItemAsync(itemId, Column.Done);
+            await _repos.MoveItemAsync(itemId, Column.Done);
             return RedirectToAction(nameof(Index));
         }
     }
