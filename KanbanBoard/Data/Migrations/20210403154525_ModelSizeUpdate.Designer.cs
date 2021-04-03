1f24a2906ac57a06fbc3b7faec76b17a028cc709
Thomas (Noit) <tba@live.dk>
2021-04-03 17:48:38 +0200

    Added database-migration.

--- KanbanBoard/Data/Migrations/20210403154525_ModelSizeUpdate.Designer.cs
+++ KanbanBoard/Data/Migrations/20210403154525_ModelSizeUpdate.Designer.cs
@@ -4,14 +4,16 @@ using KanbanBoardMVCApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KanbanBoardMVCApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210403154525_ModelSizeUpdate")]
+    partial class ModelSizeUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
