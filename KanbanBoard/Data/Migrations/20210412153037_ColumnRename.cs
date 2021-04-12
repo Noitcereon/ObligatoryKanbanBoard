1c7a5527856a2419d39364fe9a3c9e0152221586
Thomas (Noit) <tba@live.dk>
2021-04-12 17:32:50 +0200

    ColumRename migration. Hopefully final migration.

---
+++ KanbanBoard/Data/Migrations/20210412153037_ColumnRename.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KanbanBoardMVCApp.Data.Migrations
+{
+    public partial class ColumnRename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn("IdKBColumn", "KanbanColumns", "KBColumnId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
